f5d380e1f4450031a37db9607b74b3bef2d5b974
Rosivaldo Correia <rosivaldo.correia01@gmail.com>
2025-07-09 15:00:58 -0300

    atualizando pagina de cadastro.

--- src/page/Cadastro/index.js
+++ src/page/Cadastro/index.js
@@ -27,8 +27,8 @@ function Cadastro() {
     const nomeSemEspacos = nomeSanitizado.replace(/\s/g, '');
     const nomeRegex = /^[A-Za-zÀ-ÿ0-9\s]+$/;
 
-    if (!nomeSanitizado || nomeSemEspacos.length < 10 || nomeSanitizado.length > 60 || !nomeRegex.test(nomeSanitizado)) {
-      setError("Informe o nome da companhia com pelo menos 10 letras ou números e no máximo 60 caracteres.");
+    if (!nomeSanitizado || nomeSemEspacos.length < 3 || nomeSanitizado.length > 60 || !nomeRegex.test(nomeSanitizado)) {
+      setError("Informe o nome da companhia com pelo menos 3 letras ou números e no máximo 60 caracteres.");
       return;
     }
 
